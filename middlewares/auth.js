a701efaa3af551c81d2a180af7b2df4ea26525c7
megychen <megy.chen203@gmail.com>
2018-08-22 15:52:43 +0800

    Fix admin user check bug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,7 @@ var UserModel = require('../models/user');
 module.exports.adminRequired = (req, res, next) => {
   // 此处需要传user_id做判断
   var authToken = req.cookies[config.cookieName] || '';
+  var isAdmin = JSON.parse(authToken)['isAdmin']
 
   if (!authToken) {
     var err = new Error('需要登录');
@@ -12,7 +13,7 @@ module.exports.adminRequired = (req, res, next) => {
     return;
   }
 
-  if (!authToken.isAdmin) {
+  if (!isAdmin) {
     var err = new Error('需要管理员权限');
     err.status = 403;
     next(err);
